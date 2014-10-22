e6de3bb78716715f00f228cc1774e7c1de07cdbf
citpyrc <azurenova04@gmail.com>
2014-10-21 19:50:11 -0500

    Jonathan's Scripts
    Three scripts: one for the player, another for the enemy, and a third
    for the cursor object which acts as the receiver of user input for GUI
    navigation

---
+++ CursorScript.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CursorScript : MonoBehaviour {
+	private string state;
+	private string[] options;
+	private int pointerIndex;
+	public List<PlayerScript> hero;
+	public List<EnemyScript> enemy;
+
+	// Use this for initialization
+	void Start () 
+	{
+		hero = new List<PlayerScript>();
+		enemy = new List<EnemyScript>();
+
+		PlayerScript playerSc = (PlayerScript) GameObject.Find("Player").GetComponent<PlayerScript>();
+		EnemyScript enemySc = (EnemyScript) GameObject.Find("Enemy").GetComponent<EnemyScript>();
+
+		hero.Add(playerSc);
+		enemy.Add(enemySc);
+
+		state = "MenuSelect";
+
+		//Change the length of options later to accommodate more menu options
+		options = new string[1];
+		options[0] = "Attack";
+		//Add more menu options here later as string elements of the options array
+
+		pointerIndex = 0; //current menu option to point at
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (state == "MenuSelect" && Input.GetKeyDown(KeyCode.Return) && options[pointerIndex] == "Attack")
+		{
+			//Standby state is used while the interface is waiting for an attack to complete
+			state = "Standby";
+
+			PlayerScript commandedCharacter = hero[0];
+			EnemyScript enemyToAttack = enemy[0];
+
+
+			hero.RemoveAt (0);
+
+			hero.Add (commandedCharacter);
+
+			commandedCharacter.PerformAttack(enemyToAttack);
+		}
+
+		else if (state == "MenuSelect" && Input.GetAxis("Vertical") < 0)
+		{
+			//cycle downward in the options array
+			pointerIndex = (pointerIndex + 1)%options.Length;
+		}
+		else if (state == "MenuSelect" && Input.GetAxis("Vertical") > 0)
+		{
+			//cycle upward in the options array
+			pointerIndex = (options.Length + pointerIndex - 1)%options.Length;
+		}
+	}
+
+	public void WakeUp()
+	{
+		//This is called by the attacking character's script to notify the cursor to return to default state
+		state = "MenuSelect";
+	}
+}
---
+++ PlayerScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	private int attackTimer;
+	public CharacterController heroControl;
+	private Vector3 prevLocation;
+	private string state;
+	private bool successfullyAttacked;
+	private Vector3 attackDestination;
+	private EnemyScript attackTarget;
+	private CursorScript uiScript;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		uiScript = (CursorScript) GameObject.Find("Menu Cursor").GetComponent<CursorScript>();
+		state = "Standby";
+		prevLocation = transform.position;
+		speed = 15.0f;
+		successfullyAttacked = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (state == "Attacking")
+		{
+
+			if (successfullyAttacked)
+			{
+				transform.position = Vector3.MoveTowards (transform.position, prevLocation, speed*Time.deltaTime);
+				if (Vector3.Distance (transform.position, prevLocation) <= 0)
+				{
+					//made it back to starting position
+					uiScript.WakeUp ();
+					state = "Standby";
+				}
+			}
+			else
+			{
+
+
+
+				if (Vector3.Distance (transform.position, attackDestination) < 1)
+				{
+					//attack animation stuff happens here
+
+					successfullyAttacked = true;
+
+					//damage stuff happens here
+				}
+				else
+				{
+					transform.position = Vector3.MoveTowards (transform.position, attackDestination, speed*Time.deltaTime);
+				}
+			}
+		}
+	}
+	
+	public void PerformAttack(EnemyScript target)
+	{
+
+
+		successfullyAttacked = false;
+		state = "Attacking";
+		prevLocation = transform.position;
+		attackTarget = target;
+		attackDestination = target.transform.position;
+	}
+}
